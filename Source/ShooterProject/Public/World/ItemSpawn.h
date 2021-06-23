03f1b6c3de389f07a7b4f5c9015faa3ba81545b9
aCeTotal <lars.oksendal@gmail.com>
2021-06-23 03:49:38 +0200

    Lagt til kode for respawn av Items. Dette er noe som også skal brukes til å respawn kjøretøy senere. Lagde en test blueprint. Det fungerte.

--- Source/ShooterProject/Public/World/ItemSpawn.h
+++ Source/ShooterProject/Public/World/ItemSpawn.h
@@ -30,5 +30,33 @@ UCLASS()
 class SHOOTERPROJECT_API AItemSpawn : public ATargetPoint
 {
 	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, Category = "Loot")
+	class UDataTable* LootTable;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Loot")
+	TSubclassOf<class APickup> PickupClass;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Loot")
+	FIntPoint RespawnRange;
+
+public:
+	//Sets default values for this actor
+	AItemSpawn();
+
+protected:
+
+	FTimerHandle TimerHandle_RespawnItem;
+
+	UPROPERTY()
+	TArray<AActor*> SpawnedPickups;
+
+	//This is bound to the item being destroyed, so we can queue up another item to be spawned in
+	UFUNCTION()
+	void OnItemTaken(AActor* DestroyedActor);
+
+	UFUNCTION()
+	void SpawnItem();
 	
+	virtual void BeginPlay() override;
 };
